01caef2977f656b91adf2e6d2c0d3ab41d684655
Jung Su <wjdwjdtn92@naver.com>
2023-02-05 12:37:36 +0900

    🔧 Add eslint config file - jumak-dev#1

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+/**
+ * eslint 옵션
+ * - https://eslint.org/docs/latest/use/core-concepts
+ */
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+  },
+};
